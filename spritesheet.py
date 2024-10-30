cd247947539948b9361a45e085e370a453912189
UKAOBASI CALLISTUS <callistus406@gmail.com>
2024-10-30 23:28:49 +0100

    Update spritesheet.py

--- spritesheet.py
+++ spritesheet.py
@@ -1,13 +1,12 @@
-
 import pygame
+
+
 class SpriteSheet():
-    def __init__(self,image):
+    def __init__(self, image):
         self.sheet = image
-    def get_image (self,frame,width,height):
-        image = pygame.Surface((width,height)).convert_alpha()
-        image.blit(self.sheet,(0,0), ((frame * width),0,width,height))
-    
+
+    def get_image(self, frame_x,frame_y, width, height):
+        image = pygame.Surface((width, height)).convert_alpha()
+        image.blit(self.sheet, (0, 0),     ((frame_x * width), frame_y*height, width, height))
         return image
 
-
-# 86x84
